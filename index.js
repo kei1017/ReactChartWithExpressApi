608b6ad85021ec65a6ce7a0aba047ede7fd496b5
K <hightech0204@gmail.com>
2022-02-15 18:26:05 +0800

    code clean-up

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const db = require('./queries')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const db = require("./queries");
 
-const app = express()
-const port = 3030
+const app = express();
+const port = 3030;
 
-app.use(bodyParser.json())
+app.use(express.json());
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true,
   })
-)
-app.use(cors({
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-  origin: '*'
-}))
+);
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    origin: "*",
+  })
+);
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
-app.post('/auth', db.authUser)
+app.post("/auth", db.authUser);
 
-app.get('/user', db.getUsers)
-app.get('/user/:id', db.getUserById)
-app.post('/user', db.createUser)
-app.put('/user/:id', db.updateUser)
-app.delete('/user/:id', db.deleteUser)
+app.get("/user", db.getUsers);
+app.get("/user/:id", db.getUserById);
+app.post("/user", db.createUser);
+app.put("/user/:id", db.updateUser);
+app.delete("/user/:id", db.deleteUser);
 
-app.post('/filtered-data', db.getData)
+app.post("/filtered-data", db.getData);
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
--- queries.js
+++ queries.js
@@ -1,131 +1,137 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'ExpRestData',
-  password: 'abc123',
+  user: "postgres",
+  host: "localhost",
+  database: "ExpRestData",
+  password: "abc123",
   port: 5432,
-})
+});
 
 // auth apis
 
 const getUsers = (req, res) => {
-  pool.query('SELECT * FROM accounts ORDER BY id ASC', (error, results) => {
+  pool.query("SELECT * FROM accounts ORDER BY id ASC", (error, results) => {
     if (error) {
-      res.status(400).send({message: error.toString()});
+      res.status(400).send({ message: error.toString() });
     } else {
-      res.status(200).json(results.rows)
+      res.status(200).json(results.rows);
     }
-  })
-}
+  });
+};
 
 const getUserById = (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = parseInt(req.params.id);
 
-  pool.query('SELECT * FROM accounts WHERE id = $1', [id], (error, results) => {
+  pool.query("SELECT * FROM accounts WHERE id = $1", [id], (error, results) => {
     if (error) {
-      res.status(400).send({message: error.toString()});
+      res.status(400).send({ message: error.toString() });
     } else {
-      res.status(200).json(results.rows)
+      res.status(200).json(results.rows);
     }
-  })
-}
+  });
+};
 
 const createUser = (req, res) => {
-  const { username, email, password } = req.body
+  const { username, email, password } = req.body;
 
   pool.query(
-    'INSERT INTO accounts (username, email, password) VALUES ($1, $2, $3)',
+    "INSERT INTO accounts (username, email, password) VALUES ($1, $2, $3)",
     [username, email, password],
     (error, results) => {
       if (error) {
-        res.status(400).send({message: error.toString()});
+        res.status(400).send({ message: error.toString() });
       } else {
-        console.log(results)
-        res.status(201).send(`User added`)
+        console.log(results);
+        res.status(201).send(`User added`);
       }
     }
-  )
-}
+  );
+};
 
 const updateUser = (req, res) => {
-  const id = parseInt(req.params.id)
-  const { username, email, password } = req.body
+  const id = parseInt(req.params.id);
+  const { username, email, password } = req.body;
 
   pool.query(
-    'UPDATE accounts SET username = $1, email = $2, password = $3 WHERE id = $4',
+    "UPDATE accounts SET username = $1, email = $2, password = $3 WHERE id = $4",
     [username, email, password, id],
     (error, results) => {
       if (error) {
-        res.status(400).send({message: error.toString()});
+        res.status(400).send({ message: error.toString() });
       } else {
-        res.status(200).send(`User modified with ID: ${id}`)
+        res.status(200).send(`User modified with ID: ${id}`);
       }
     }
-  )
-}
+  );
+};
 
 const deleteUser = (req, res) => {
-  const id = parseInt(req.params.id)
+  const id = parseInt(req.params.id);
 
-  pool.query('DELETE FROM accounts WHERE id = $1', [id], (error, results) => {
+  pool.query("DELETE FROM accounts WHERE id = $1", [id], (error, results) => {
     if (error) {
-      res.status(400).send({message: error.toString()});
+      res.status(400).send({ message: error.toString() });
     } else {
-      res.status(200).send(`User deleted with ID: ${id}`)
+      res.status(200).send(`User deleted with ID: ${id}`);
     }
-  })
-}
+  });
+};
 
 const authUser = (req, res) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
   pool.query(
-    'SELECT * FROM accounts WHERE email = $1 AND password = $2',
+    "SELECT * FROM accounts WHERE email = $1 AND password = $2",
     [email, password],
     (error, results) => {
       if (error) {
-        res.status(400).send({message: error.toString()});
-      } if (results.rows.length > 0) {
-        res.status(200).send(results.rows.length > 0 ? results.rows[0].username : "");
+        res.status(400).send({ message: error.toString() });
+      }
+      if (results.rows.length > 0) {
+        res
+          .status(200)
+          .send(results.rows.length > 0 ? results.rows[0].username : "");
       } else {
-        res.status(400).send({message: 'Invalid email or wrong password.'});
+        res.status(400).send({ message: "Invalid email or wrong password." });
       }
     }
-  )
-}
+  );
+};
 
 // filtering apis
 
 const getData = (req, res) => {
-  const { filter1, filter2 } = req.body
+  const { filter1, filter2 } = req.body;
 
-  let filters = '1=1'
+  let filters = "1=1";
   if (!!filter1) {
-    filters = `${filters} AND name='${filter1}'`
+    filters = `${filters} AND name='${filter1}'`;
   }
   if (!!filter2) {
-    switch(filter2) {
-      case 'gte-10000':
-        filters = `${filters} AND amount>=10000`
+    switch (filter2) {
+      case "gte-10000":
+        filters = `${filters} AND amount>=10000`;
         break;
-      case 'gte-1000':
-        filters = `${filters} AND amount>=1000`
+      case "gte-1000":
+        filters = `${filters} AND amount>=1000`;
         break;
-      case 'lt-1000':
-        filters = `${filters} AND amount<1000`
+      case "lt-1000":
+        filters = `${filters} AND amount<1000`;
         break;
     }
   }
 
-  pool.query(`SELECT * FROM deposits WHERE ${filters} ORDER BY record_time ASC`, (error, results) => {
-    if (error) {
-      res.status(400).send({message: error.toString()});
-    } else {
-      res.status(200).json(results.rows)
+  pool.query(
+    `SELECT * FROM deposits WHERE ${filters} ORDER BY record_time ASC`,
+    (error, results) => {
+      if (error) {
+        res.status(400).send({ message: error.toString() });
+      } else {
+        res.status(200).json(results.rows);
+      }
     }
-  })
-}
+  );
+};
 
 module.exports = {
   getUsers,
@@ -135,4 +141,4 @@ module.exports = {
   deleteUser,
   authUser,
   getData,
-}
+};
